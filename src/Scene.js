3eb95aeccc2afebec54c01dba00e7c7bbd25b44a
Adam Friedman <adam@blok.party>
2020-07-23 11:09:11 -0700

    Added global variables

--- src/Scene.js
+++ src/Scene.js
@@ -3,6 +3,19 @@ import './App.css';
 
 export default class Scene extends Component {
 	componentDidMount() {
+		const canvas = document.getElementById('canvas');
+		const ctx = canvas.getContext('2d');
+		canvas.height = window.innerHeight;
+		canvas.width = window.innerWidth;
+
+		// load images
+		const images = {};
+		images.player = new Image();
+		images.player.src = 'https://i.ibb.co/Ybk7y06/character.png';
+		const characterActions = ['up', 'top right', 'right', 'down right', 'down'];
+		const numberOfCharacters = 25;
+		const characters = [];
+
 		class Character {
 			constructor() {}
 			draw() {
